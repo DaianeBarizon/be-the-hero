53381bc036e726c8f1cbf3ca29bc94590fdbdb90
DaianeBarizon <daiane.barizon@neki-it.com.br>
2020-04-19 20:17:30 -0300

    Paginando e inserindo cors

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,23 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(request, response) {
-    const incidents = await connection("incidents").select("*");
+    const { page = 1 } = request.query;
+    const [count] = await connection("incidents").count();
+
+    const incidents = await connection("incidents")
+      .join("ongs", "ong_id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf",
+      ]);
+
+    response.header("X-Total-Count", count["count(*)"]);
     return response.json(incidents);
   },
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express(); //instanciar aplicação
 
+app.use(cors());
 app.use(express.json()); //Antes de todas as requisições tornar JSON
 app.use(routes);
 
